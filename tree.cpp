cef10770d8045e79c0f67e1994350f72627a2078
Phaneendra293 <85346660+Phaneendra293@users.noreply.github.com>
2025-08-20 22:41:04 +0530

    Add files via upload

---
+++ tree.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+// Insert node into BST
+Node* insert(Node* root, int val) {
+    if (root == nullptr) {
+        return new Node(val);
+    }
+    if (val < root->data) {
+        root->left = insert(root->left, val);
+    } else {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+// Traversals
+void preorder(Node* root) {
+    if (root == nullptr) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node* root) {
+    if (root == nullptr) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (root == nullptr) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    Node* root = nullptr;
+    int n, val;
+
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    cout << "Enter values: ";
+    for (int i = 0; i < n; i++) {
+        cin >> val;
+        root = insert(root, val);
+    }
+
+    cout << "\nPreorder Traversal: ";
+    preorder(root);
+
+    cout << "\nInorder Traversal: ";
+    inorder(root);
+
+    cout << "\nPostorder Traversal: ";
+    postorder(root);
+
+    cout << endl;
+    return 0;
+}
